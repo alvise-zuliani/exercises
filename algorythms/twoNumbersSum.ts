2d927a9c26f9f24aa63066dcbc93e3c082bb68af
Alvise Zuliani <alvise.zul@gmail.com>
2025-05-09 19:16:12 +0200

    two numbers sum

--- algorythms/twoNumbersSum.ts
+++ algorythms/twoNumbersSum.ts
@@ -8,13 +8,65 @@
 //
 // You can assume that there will be at most one pair of numbers summing up to the target sum.
 
+// Time Complexity: O(n)T, O(n)S
 export function twoNumberSum(array: number[], targetSum: number) {
-    // Write your code here.
+    const seenArrayMembers: Map<number, boolean> = new Map();
     for (let i: number = 0; i < array.length; i++) {
-        if (array.includes(targetSum - array[i]) && array[i] !== (targetSum - array[i])) {
-            return [targetSum - array[i], array[i]]
+        const currentNum = array[i];
+        const currentComplementary: number = targetSum - currentNum;
+        // check for a complementary in the seen array members
+        if (seenArrayMembers.has(currentComplementary)){
+            return [currentNum, currentComplementary];
         }
+        // add to seen array members
+        seenArrayMembers.set(currentNum, true);
     }
+    // return default empty array
     return [];
 }
-  
+    
+// Time Complexity: O(n^2)T, O(1)S
+export function twoNumberSum(array: number[], targetSum: number) {
+
+    for (let i: number = 0; i < array.length; i++) {
+        const currentNum: number = array[i];
+        const complementary: number = targetSum - currentNum;
+        if array.contains(complementary){
+            return [currentNum, complementary]
+        }
+    }
+    // return default empty array
+    return [];
+}
+
+// Complexity: O(n*log(n))T; O(1)S
+export function twoNumberSum(array: number[], targetSum: number) {
+    // we sort the array in an ascending order
+    array.sort((a, b) => a - b);
+
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left < right) {
+        // assign indices to head and tail
+        let head: number = array[left];
+        let tail: number = array[right];
+
+        // since the array is sorted, we can move the tail or
+        // head to check for matches by comparing to the target
+        // number, knowing that moving to the right means the 
+        // sum will be higher, while moving to the left means
+        // the opposite.
+        if (head + tail === targetSum) {
+            return [head, tail];
+        } else if (head + tail > targetSum) {
+            // sum is too high, we need to move the tail back
+            right --;
+        } else if (head + tail < targetSum) {
+            // sum is too low, we need to move the head forward
+            left ++;
+        }
+    }
+    // return default empty array
+    return [];
+}   
