e2732399333aab6e9dd0d9eee8cb1e8d28e49974
Alvise Zuliani <alvise.zul@gmail.com>
2025-06-08 21:05:26 +0200

    tournamentWinner

--- algorythms/js/easy/validateSubsequence.ts
+++
@@ -1,9 +0,0 @@
-// Given two non-empty arrays of integers, write a function that determines whether the second array is a
-// subsequence of the first one. Note that a single number in an array and the array itself are both valid
-// subsequences of the array
-
-export function isValidSubsequence(array: number[], sequence: number[]) {
-    // Write your code here.
-    return 
-}
-  
